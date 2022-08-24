b272bdf40eec9e1845d930f0e2301865692997c4
Thomas <61391200+Nekromast@users.noreply.github.com>
2022-08-24 19:14:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import os
 
 WIDTH, HEIGHT = 1000, 720
 SHIPWIDTH, SHIPHEIGHT = 80, 100
+SPEED = 5
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
 PLAYERSHIP = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'ship.png')), (SHIPWIDTH, SHIPHEIGHT))
 BACKGROUND = pygame.image.load(os.path.join('assets', 'background.gif'))
+
+def movement(keys_pressed, player):
+    if keys_pressed[pygame.K_w] and player.y - SPEED > 0:  # oben
+        player.y -= SPEED
+    if keys_pressed[pygame.K_a] and player.x + SPEED > 0:  # links
+        player.x -= SPEED
+    if keys_pressed[pygame.K_s] and player.y - SPEED < HEIGHT - player.height*2:  # unten
+        player.y += SPEED
+    if keys_pressed[pygame.K_d] and player.x + SPEED + player.width*2 < WIDTH:  # rechts
+        player.x += SPEED
 
 def draw_window(player):
     WINDOW.fill((0, 0, 0)) # black background
@@ -27,6 +38,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                
+        keys_pressed = pygame.key.get_pressed()
+        movement(keys_pressed, player)
 
         draw_window(player)
 
