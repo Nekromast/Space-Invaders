dcb021a2a7699fd91acc549e896aa472be655ff0
Mike Esteves <mikefmesteves@gmail.com>
2022-08-24 17:32:34 +0200

    sus

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+sus
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
